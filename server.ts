bd93a2838099a7ccab198352eff9e115c0d03fe0
SOURADEEP HAZRA <105676331+Souradeephazra123@users.noreply.github.com>
2026-07-03 14:03:57 +0530

    Added now two script

--- server.ts
+++ server.ts
@@ -235,28 +235,6 @@ app.post("/api/run", async (req, res) => {
   });
 });
 
-// // Configure Vite middleware / asset serving
-// const startServer = async () => {
-//   if (process.env.NODE_ENV !== 'production') {
-//     const vite = await createViteServer({
-//       server: { middlewareMode: true },
-//       appType: 'spa',
-//     });
-//     app.use(vite.middlewares);
-//   } else {
-//     const distPath = path.join(process.cwd(), 'dist');
-//     app.use(express.static(distPath));
-//     app.get('*', (req, res) => {
-//       res.sendFile(path.join(distPath, 'index.html'));
-//     });
-//   }
-
-//   app.listen(PORT, '0.0.0.0', () => {
-//     console.log(`Server running on http://localhost:${PORT}`);
-//   });
-// };
-
-// startServer();
 
 // GitHub OAuth authorization URL endpoint
 app.get("/api/github/auth", (req, res) => {
